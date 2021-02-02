b53d8b42d1c87b4527115d834bb3b7ee5b63a774
HemaMathiyalagan <31774707+HemaMathiyalagan@users.noreply.github.com>
2021-02-02 20:58:23 +0530

    Update JugglingArrayRotation.java

--- JugglingArrayRotation.java
+++ JugglingArrayRotation.java
@@ -1,40 +1,39 @@
 package com.hema.array;
 
 public class JugglingArrayRotation {
-public static void main(String args[])
-{
-int d=2;
-int a[]= {1,2,3,4,5,6};
-arrRotate(a,d,a.length);
-for(int i=0;i<a.length;i++)
-	System.out.println(a[i]);
-}
-public static void arrRotate(int[] a,int d,int len)
-{
-int gcd=gcd(d,len);
-int temp,j,k,i;
-for(i=0;i<gcd;i++)
-{
-	temp=a[i];
-	j=i;
-	while(true)
+	public static void main(String args[])
 	{
-		k=j+d;
-		if(k>=len)
-			k=k-len;
-		if(i==k)
-			break;
-		a[j]=a[k];
-		j=k;
+		int d=8;
+		int a[]= {1,2,3,4,5,6,7};
+		arrRotate(a,d,a.length);
+		for(int i=0;i<a.length;i++)
+			System.out.println(a[i]);
 	}
-	a[j]=temp;
-}
-}
-public static int gcd(int a,int b)
-{
-if(b==0)
-	return a;
-else
-	return gcd(b,a%b);
-}
+	public static int gcd(int a,int b)
+	{
+		if(b==0)
+			return a;
+		else
+			return gcd(b,a%b);
+	}
+	public static void arrRotate(int a[],int d, int l)
+	{
+		d=d%l;
+		int i,j,k,temp;
+		for(i=0;i<gcd(d,l);i++)
+		{
+			temp=a[i];
+			j=i;
+			while(true)
+			{
+				k=j+d;
+				if(k>=l)
+					k=k-l;
+				if(k==i)
+					break;
+				a[j]=a[k];
+				j=k;
+			}
+		}
+	}
 }
